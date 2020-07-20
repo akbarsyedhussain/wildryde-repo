a01e82f22232052ce734b6665b65e698a9639418
akbarsyedhussain <68562715+akbarsyedhussain@users.noreply.github.com>
2020-07-20 15:07:10 -0400

    Add files via upload

---
+++ website/js/config.js
@@ -0,0 +1,10 @@
+window._config = {
+    cognito: {
+        userPoolId: 'ca-central-1_TrXSY2Dp1', // e.g. ca-central-1_TrXSY2Dp1
+        userPoolClientId: '4hqbsacjkvqivvj60t9f2l4t11', // e.g. 4hqbsacjkvqivvj60t9f2l4t11
+        region: 'ca-central-1' // e.g. ca-central-1
+    },
+    api: {
+        invokeUrl: 'https://2nmfxdngp0.execute-api.ca-central-1.amazonaws.com/prod' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod,
+    }
+};
